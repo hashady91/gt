1d56f8d375ea8f77d4df83829f83a41e3cb7c8d0
hashady91 <hashady91@gmail.com>
2014-03-09 10:41:36 +0700

    Register and login user

--- application/modules/user/controllers/IndexController.php
+++ application/modules/user/controllers/IndexController.php
@@ -14,5 +14,49 @@ class User_IndexController extends Cl_Controller_Action_UserIndex
 	public function anyOtherRequestAction()
 	{
 		
-	}   
+	}
+
+	public function loginAction()
+	{
+		parent::loginAction();
+		if (Zend_Registry::isRegistered('authentication_happened'))
+		{
+			//set one more cookie. _cl_is_admin
+			$user = Zend_Registry::get('user');
+			if (has_perm('admin_story'))
+				set_cookie('is_admin', 1);
+		}
+		
+		Bootstrap::$pageTitle = t('login_signup', 2);
+	}
+	
+	public function registerAction()
+	{
+		parent::registerAction();
+		Bootstrap::$pageTitle = t('login_signup', 2);
+	}
+	
+	public function logoutAction(){
+	//clear identity & reset cookie
+		$adapter = new Cl_Auth_Adapter_PersistentDb();
+		$r = $adapter->clearIdentity();
+		$r = array('success' => true);
+		
+		set_cookie('is_admin','', -3600);
+		
+		if(is_ajax()) {
+			send_json($r);
+		}
+		else
+		{
+			if(!$r['success']){
+				// return error;
+				$this->setViewParam('err', $r['err']);
+			}
+			else {
+				// redirect to homepage here?
+				$this->_redirect("/");
+			}
+		}
+	}
 }
