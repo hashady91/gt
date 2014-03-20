ee812fb05d1bb5d003789999c4ed014aa14a523f
hashady91 <hashady91@gmail.com>
2014-03-20 12:19:05 +0100

    redefine production

--- application/configs/defines.production.php
+++ application/configs/defines.production.php
@@ -3,10 +3,11 @@
  * SITE specifics
  */
 //define('POST_PAGE_SIZE', 30);
-define('SITE_URL', 'http://q.fcabal.com');
+define('SITE_URL', 'http://q.fcabal.net');
 //define('SITE_URL', 'http://www.oakq.com');
 define('SAND_ASSETS_CDN', 'http://sandassets.fcabal.net/');
-define('ASSETS_CDN', 'http://assets.fcabal.net');
+define('ASSETS_CDN', 'https://assets.fcabal.net/');
+define('STATIC_GT', 'http://static.fcabal.net/');
 
 define('NOREPLY_EMAIL', 'noreply@fcabal.net');
 define('DOMAIN', 'q.fcabal.net');
@@ -20,7 +21,7 @@ define('PAGE_RANGES', 7);//numbers pagination links at a time
  * DB connections
  */
 //include_once('functions.php');
-define('MONGO_HOST', 'localhost');
+define('MONGO_HOST', '127.0.0.1');
 //define('DICT_BACKEND', 'mongo');
 define('DICT_BACKEND', 'redis');
 define('REDIS_PORT', 6379);
--- public/index.php
+++ public/index.php
@@ -5,8 +5,8 @@ ini_set('display_errors', false);
 */
 //$a = time() + microtime();
 
-echo "Website đang trong quá trình xây dựng.Mong mọi người thông cảm!";
-die();
+//echo "Underconstruction -  come back soon";
+//die();
 define('bootstrap3', 1);
 
 defined('PUBLIC_PATH')
